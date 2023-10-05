536f5e4b3e65d1645af7b3b3cd66f32bae5a7815
timewarpsgh <515441483@qq.com>
2023-10-05 13:46:25 +0800

    refactor PacketHandler on server OK

--- src/server/packet_handler.py
+++ src/server/packet_handler.py
@@ -13,25 +13,20 @@ class PacketHandler:
         self.session = session
 
     def handle_packet(self, packet):
+        packet_name = type(packet).__name__
+        getattr(self, f'_handle_{packet_name}')(packet)
 
-        if isinstance(packet, Login):
-            self.__handle_login(packet)
-        elif isinstance(packet, NewAccount):
-            self.__handle_new_account(packet)
-        elif isinstance(packet, NewRole):
-            self.__handle_new_role(packet)
-
-    def __handle_login(self, login):
+    def _handle_Login(self, login):
         login_res = LoginRes()
         login_res.login_res_type = LoginRes.LoginResType.OK
         self.session.send(login_res)
 
-    def __handle_new_account(self, new_account):
+    def _handle_NewAccount(self, new_account):
         new_account_res = NewAccountRes()
         new_account_res.new_account_res_type = NewAccountRes.NewAccountResType.OK
         self.session.send(new_account_res)
 
-    def __handle_new_role(self, new_role):
+    def _handle_NewRole(self, new_role):
         new_role_res = NewRoleRes()
         new_role_res.new_role_res_type = NewRoleRes.NewRoleResType.OK
         self.session.send(new_role_res)

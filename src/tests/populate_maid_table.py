e15fb5eec5fb9f6ed1ead40dd92467914ea9cd39
timewarpsgh <515441483@qq.com>
2024-08-05 11:26:45 +0800

    populate maid table OK

---
+++ src/tests/populate_maid_table.py
@@ -0,0 +1,136 @@
+import sys
+import os
+
+PATH_TO_DIR = r'D:\data\code\python\project_mount_christ\src\server'
+sys.path.append(PATH_TO_DIR)
+
+from world_models import SESSION, Maid
+
+
+hash_maids = {
+    1: {
+        'name': 'Layla',
+        'image': [4, 8],
+    },
+    2: {
+        'name': 'Melanie',
+        'image': [6, 7],
+    },
+    3: {
+        'name': 'Johanna',
+        'image': [1, 8],
+    },
+    4: {
+        'name': 'Tisa',
+        'image': [5, 8],
+    },
+    5: {
+        'name': 'Helen',
+        'image': [2, 8],
+    },
+    6: {
+        'name': 'Titis',
+        'image': [8, 8],
+    },
+    7: {
+        'name': 'Leticia',
+        'image': [7, 7],
+    },
+    8: {
+        'name': 'Elaine',
+        'image': [11, 7],
+    },
+    9: {
+        'name': 'Jamila',
+        'image': [6, 7],
+    },
+    10: {
+        'name': 'Mei-Yi',
+        'image': [16, 8],
+    },
+    11: {
+        'name': 'Theresa',
+        'image': [12, 7],
+    },
+    12: {
+        'name': 'Aruna',
+        'image': [3, 8],
+    },
+    13: {
+        'name': 'Claudia',
+        'image': [5, 7],
+    },
+    14: {
+        'name': 'Isabella',
+        'image': [7, 8],
+    },
+    15: {
+        'name': 'Ladia',
+        'image': [13, 8],
+    },
+    16: {
+        'name': 'Lucia',
+        'image': [3, 7],
+    },
+    17: {
+        'name': 'Lillian',
+        'image': [8, 7],
+    },
+    18: {
+        'name': 'Rukia',
+        'image': [11, 8],
+    },
+    19: {
+        'name': 'Mathilda',
+        'image': [15, 7],
+    },
+    20: {
+        'name': 'Lupe',
+        'image': [9, 8],
+    },
+    21: {
+        'name': 'Salma',
+        'image': [5, 8],
+    },
+    22: {
+        'name': 'Onatsu',
+        'image': [15, 8],
+    },
+    23: {
+        'name': 'Natasha',
+        'image': [14, 7],
+    },
+    24: {
+        'name': 'Silvia',
+        'image': [10, 8],
+    },
+    25: {
+        'name': 'Tobia',
+        'image': [4, 7],
+    },
+    26: {
+        'name': 'Shani',
+        'image': [16, 7],
+    },
+    27: {
+        'name': 'Viveka',
+        'image': [9, 7],
+    },
+    28: {
+        'name': 'Francesca',
+        'image': [13, 7],
+    },
+}
+
+for id, dict in hash_maids.items():
+    name = dict['name']
+    img_id = str(dict['image'][0]) + '_' + str(dict['image'][1])
+
+    new_obj = Maid(
+        name=name,
+        img_id=img_id,
+    )
+
+    SESSION.add(new_obj)
+
+SESSION.commit()

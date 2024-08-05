7b3f4160d7caa0cfb9ce842ea5c5cebd88e166aa
timewarpsgh <515441483@qq.com>
2024-08-05 11:40:11 +0800

    init port model

--- src/server/models/world_models.py
+++ src/server/models/world_models.py
@@ -20,6 +20,35 @@ def create_session():
 SESSION = create_session()
 
 
+class Port(BASE):
+
+	__tablename__ = 'port'
+
+	id = Column(Integer, primary_key=True)
+
+	name = Column(String(20))
+	x = Column(Integer)
+	y = Column(Integer)
+
+	maid_id = Column(Integer)
+	mate_id = Column(Integer)
+
+	economy = Column(Integer)
+	industry = Column(Integer)
+
+	invest_from_nations = Column(String)
+
+	region_id = Column(Integer)
+	economy_id = Column(Integer)
+	industry_id = Column(Integer)
+	items_ids = Column(String)
+
+	building_locations = Column(String)
+
+	tile_set = Column(Integer)
+	governer_role_id = Column(Integer)
+
+
 class Maid(BASE):
 	# table
 	__tablename__ = 'maid'

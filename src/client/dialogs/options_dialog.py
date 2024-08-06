921913a13901ec6b2f93265610dce6896fa5bf1a
timewarpsgh <515441483@qq.com>
2024-08-06 17:30:31 +0800

    show other building menus OK

--- src/client/dialogs/options_dialog.py
+++ src/client/dialogs/options_dialog.py
@@ -54,20 +54,157 @@ class OptionsDialog:
             mgr=self.mgr
         )
 
+    def __show_bar_menu(self):
+        option_2_callback = {
+            'Recruit Crew': '',
+            'Dismiss Crew': '',
+            'Treat': '',
+            'Meet': '',
+            'Fire Mate': '',
+            'Waitress': '',
+        }
+
+        MyMenuWindow(
+            title='',
+            option_2_callback=option_2_callback,
+            mgr=self.mgr
+        )
+
+    def __show_dry_dock_menu(self):
+        option_2_callback = {
+            'New Ship': '',
+            'Used Ship': '',
+            'Repair': '',
+            'Sell': '',
+            'Remodel': '',
+        }
+
+        MyMenuWindow(
+            title='',
+            option_2_callback=option_2_callback,
+            mgr=self.mgr
+        )
+
+    def __show_harbor_menu(self):
+        option_2_callback = {
+            'Sail': '',
+            'Load Supply': '',
+            'Unload Supply': '',
+        }
+
+        MyMenuWindow(
+            title='',
+            option_2_callback=option_2_callback,
+            mgr=self.mgr
+        )
+
+    def __show_inn_menu(self):
+        option_2_callback = {
+            'Check In': '',
+            'Gossip': '',
+            'Port Info': '',
+            'Walk Around': '',
+        }
+
+        MyMenuWindow(
+            title='',
+            option_2_callback=option_2_callback,
+            mgr=self.mgr
+        )
+
+    def __show_palace_menu(self):
+        option_2_callback = {
+            'Meet Ruler': '',
+            'Defect': '',
+            'Gold Aid': '',
+            'Ship Aid': '',
+        }
+
+        MyMenuWindow(
+            title='',
+            option_2_callback=option_2_callback,
+            mgr=self.mgr
+        )
+
+    def __show_job_house_menu(self):
+        option_2_callback = {
+            'Job Assignment': '',
+            'Country Info': '',
+        }
+
+        MyMenuWindow(
+            title='',
+            option_2_callback=option_2_callback,
+            mgr=self.mgr
+        )
+
+    def __show_bank_menu(self):
+        option_2_callback = {
+            'Check Balance': '',
+            'Deposit': '',
+            'Withdraw': '',
+            'Borrow': '',
+            'Repay': '',
+        }
+
+        MyMenuWindow(
+            title='',
+            option_2_callback=option_2_callback,
+            mgr=self.mgr
+        )
+
+    def __show_item_shop_menu(self):
+        option_2_callback = {
+            'Buy': '',
+            'Sell': '',
+        }
+
+        MyMenuWindow(
+            title='',
+            option_2_callback=option_2_callback,
+            mgr=self.mgr
+        )
+
+    def __show_church_menu(self):
+        option_2_callback = {
+            'Pray': '',
+            'Donate': '',
+        }
+
+        MyMenuWindow(
+            title='',
+            option_2_callback=option_2_callback,
+            mgr=self.mgr
+        )
+
+    def __show_fortune_house_menu(self):
+        option_2_callback = {
+            'Life': '',
+            'Career': '',
+            'Love': '',
+            'Mates': '',
+        }
+
+        MyMenuWindow(
+            title='',
+            option_2_callback=option_2_callback,
+            mgr=self.mgr
+        )
+
     def __show_buildings_menu(self):
         option_2_callback = {
             'Market': partial(self.__show_market_menu),
-            'Bar': '',
-            'Dry Dock': '',
-            'Harbor': '',
-            'Inn': '',
-            'Palace': '',
-            'Job House': '',
-            'Misc': '',
-            'Bank': '',
-            'Item Shop': '',
-            'Church': '',
-            'Fortune House': '',
+            'Bar': partial(self.__show_bar_menu),
+            'Dry Dock': partial(self.__show_dry_dock_menu),
+            'Harbor': partial(self.__show_harbor_menu),
+            'Inn': partial(self.__show_inn_menu),
+            'Palace': partial(self.__show_palace_menu),
+            'Job House': partial(self.__show_job_house_menu),
+            'Misc': 'test',
+            'Bank': partial(self.__show_bank_menu),
+            'Item Shop': partial(self.__show_item_shop_menu),
+            'Church': partial(self.__show_church_menu),
+            'Fortune House': partial(self.__show_fortune_house_menu),
         }
 
         MyMenuWindow(

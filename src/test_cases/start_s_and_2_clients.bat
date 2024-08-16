3467087ea09c2b075edc5238aeb6124a59db7400
timewarpsgh <515441483@qq.com>
2024-08-16 15:35:30 +0800

    start_s_and_2_clients.bat OK

---
+++ src/test_cases/start_s_and_2_clients.bat
@@ -0,0 +1,13 @@
+:: start server in another terminal
+start cmd /k python D:\data\code\python\project_mount_christ\src\server\server.py
+
+:: Sleep for 1 seconds
+ping -n 1 127.0.0.1 >nul
+echo 123
+
+:: start client 1 in another terminal
+start cmd /k python D:\data\code\python\project_mount_christ\src\client\client.py t1
+
+
+:: start client 2 in another terminal
+start cmd /k python D:\data\code\python\project_mount_christ\src\client\client.py t2

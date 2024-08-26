a1a95d2b7a9a0eafa06c19b45c4d59127bf5d571
timewarpsgh <515441483@qq.com>
2024-08-26 13:59:43 +0800

    diagnal movement speed too fast OK

--- src/client/model.py
+++ src/client/model.py
@@ -391,7 +391,7 @@ class Role:
 
     def calc_move_timer(self):
         if self.is_dir_diagnal():
-            move_timer = 1.4 * c.PIXELS_COVERED_EACH_MOVE / self.speed
+            move_timer = 1.41 * c.PIXELS_COVERED_EACH_MOVE / self.speed
         else:
             move_timer = c.PIXELS_COVERED_EACH_MOVE / self.speed
         return move_timer

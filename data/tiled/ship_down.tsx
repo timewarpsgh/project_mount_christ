dc9b336cc0585cc7ba8fdbbfd3975440085df5e4
timewarpsgh <515441483@qq.com>
2024-10-29 10:30:01 +0800

    ships at sea OK

---
+++ data/tiled/ship_down.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="ship_down" tilewidth="32" tileheight="32" tilecount="1" columns="1">
+ <image source="../imgs/ship_in_battle/4.png" width="32" height="32"/>
+</tileset>

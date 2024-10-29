f9c0fb3d55a1b4462b5a5810ca6da1831385b43c
timewarpsgh <515441483@qq.com>
2024-10-29 09:41:22 +0800

    ships in battle OK

---
+++ data/tiled/aaa.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="aaa" tilewidth="32" tileheight="32" tilecount="32" columns="8">
+ <image source="../imgs/player/ship-tileset - 副本.png" width="256" height="128"/>
+</tileset>

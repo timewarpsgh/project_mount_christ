58b990a27a4d493c8aef0fcc00940b856b4795f4
timewarpsgh <515441483@qq.com>
2024-10-30 16:37:57 +0800

    my own ship sprites OK

---
+++ data/tiled/saddgsag.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="saddgsag" tilewidth="32" tileheight="32" tilecount="1" columns="1">
+ <image source="C:/Users/陈林/Downloads/sprites_for_ship_at_sea/下载.png" width="32" height="32"/>
+</tileset>
---
+++ data/tiled/sadgasdg.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="sadgasdg" tilewidth="32" tileheight="32" tilecount="216" columns="8">
+ <image source="C:/Users/陈林/Downloads/ship_sprites_real.png" width="260" height="888"/>
+</tileset>

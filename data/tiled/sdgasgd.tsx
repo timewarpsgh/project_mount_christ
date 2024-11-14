74bf6bd288e53a40ac78b665d23dca2998e8766d
timewarpsgh <515441483@qq.com>
2024-11-14 18:03:48 +0800

    building type symbols  OK

---
+++ data/tiled/sdgasgd.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="sdgasgd" tilewidth="16" tileheight="16" tilecount="9216" columns="96">
+ <image source="../imgs/my_ports/30/day_dimmed.png" width="1536" height="1536"/>
+</tileset>

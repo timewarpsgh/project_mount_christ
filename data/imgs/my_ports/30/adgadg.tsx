287a7fd61de2d9c6457ada946eade92e14f44c24
timewarpsgh <515441483@qq.com>
2024-11-15 12:09:31 +0800

    collision OK

--- data/imgs/my_ports/30/adgadg.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="adgadg" tilewidth="16" tileheight="16" tilecount="9216" columns="96">
- <image source="day.png" width="1536" height="1536"/>
-</tileset>

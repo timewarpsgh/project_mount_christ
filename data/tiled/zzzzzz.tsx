0f2b10d00520ca1761d4e18166f1ee4489ded4c3
timewarpsgh <515441483@qq.com>
2024-11-28 16:00:07 +0800

    fleet info bg OK

---
+++ data/tiled/zzzzzz.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="zzzzzz" tilewidth="16" tileheight="16" tilecount="1024" columns="32">
+ <image source="C:/Users/陈林/Downloads/RPG Village Tileset 09-02-23/RPG Village Tileset/Village_TerrainTiles.png" width="512" height="512"/>
+</tileset>

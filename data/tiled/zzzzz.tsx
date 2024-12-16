77a581214d4b8a21e991a301d4c685ddfb5c767d
timewarpsgh <515441483@qq.com>
2024-12-16 19:33:17 +0800

    change some discovery imgs OK

---
+++ data/tiled/zzzzz.tsx
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="zzzzz" tilewidth="49" tileheight="49" tilecount="6" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="C:/Users/陈林/Downloads/Gemini_Generated_Image_4dtko14dtko14dtk.jpg" width="49" height="49"/>
+ </tile>
+ <tile id="1">
+  <image source="C:/Users/陈林/Downloads/Gemini_Generated_Image_8iofj38iofj38iof.jpg" width="49" height="49"/>
+ </tile>
+ <tile id="2">
+  <image source="C:/Users/陈林/Desktop/屏幕截图 2024-12-16 154828.png" width="49" height="49"/>
+ </tile>
+ <tile id="3">
+  <image source="C:/Users/陈林/Downloads/Gemini_Generated_Image_cz1k7wcz1k7wcz1k.jpg" width="49" height="49"/>
+ </tile>
+ <tile id="4">
+  <image source="C:/Users/陈林/Desktop/屏幕截图 2024-12-16 165940.png" width="49" height="49"/>
+ </tile>
+ <tile id="5">
+  <image source="C:/Users/陈林/Desktop/屏幕截图 2024-12-16 171322.png" width="49" height="49"/>
+ </tile>
+</tileset>

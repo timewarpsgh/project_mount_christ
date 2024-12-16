3dd46e744963ded0ce76ac29ce539c7b4ae8dd60
timewarpsgh <515441483@qq.com>
2024-12-16 10:25:44 +0800

    items images OK

--- data/tiled/ssss.tsx
+++ data/tiled/ssss.tsx
@@ -1,10 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="ssss" tilewidth="48" tileheight="48" tilecount="96" columns="12">
- <image source="C:/Users/陈林/Downloads/Itch 01/peopleA1.png" width="576" height="384"/>
- <tile id="54">
-  <animation>
-   <frame tileid="48" duration="100"/>
-   <frame tileid="50" duration="100"/>
-  </animation>
- </tile>
+<tileset version="1.10" tiledversion="1.11.0" name="ssss" tilewidth="49" tileheight="49" tilecount="128" columns="16">
+ <image source="../imgs/discoveries_and_items/discoveries_and_items.png" width="784" height="392"/>
 </tileset>
